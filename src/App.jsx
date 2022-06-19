7f7052f72d7e8f0ff84dc7343eef91c962f538a1
Nguyen Thanh Luc <luc.nguyen150198@gmail.com>
2022-06-19 09:35:20 +0700

    setup base css  html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ExamplePage from './pages/ExamplePage';
+import { clientRouter } from './configs/router.config';
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={'/exam'} component={ExamplePage} />
-        <Route exact path={'/'} component={ExamplePage} />
+        {clientRouter.map((route, index) => {
+          const { path, exact, Component } = route;
+          return (
+            <Route
+              key={index}
+              path={path}
+              exact={exact}
+              component={Component}
+            />
+          );
+        })}
       </Switch>
     </BrowserRouter>
   );
--- src/components/_common/text/text.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createClass } from 'src/utils/utils';
-
-export default function text() {
-  return <span className={createClass('btn')}></span>;
-}
